ea95729a68665637e83a7735d9cdabbe11914e91
Trey Stafford <trey.stafford@colorado.edu>
2024-04-25 16:04:03 -0600

    Init work on using parsl to run a series of commands
    * Explicitly creating subdirs for each command step seems wrong. Is there a
      better way?
    * We think data
      futures (https://parsl.readthedocs.io/en/stable/userguide/futures.html#datafutures)
      might be the "right" thing to use for chaining commands instead of calling
      `result()` for each, but that requires us to know in advance what the output
      filename of each command id. Are there cases where a command could output
      multiple files?

--- src/ogdc_runner/recipe/simple.py
+++ src/ogdc_runner/recipe/simple.py
@@ -11,53 +11,49 @@ from ogdc_runner.recipe import get_recipe_config
 environment = Environment(loader=PackageLoader("ogdc_runner"))
 template = environment.get_template("simple_recipe.py.j2")
 
-input_subkeys = {
-    "url": "beam.io.SomeTransformThatCanReadAFileFromAUrl",
-    "dataone_doi": "our_custom_transforms.DataOneDoiInput",
-}
+# TODO: get these from envvars
+PVC_NAME = "qgnet-pvc-test-1"
+MOUNT_DIR = Path("/data")
 
 
-def _get_commands(simple_recipe_path: Path) -> list[str]:
+def _get_workdir(*, mount_dir: Path, id: str) -> Path:
+    return mount_dir / id
+
+
+def _get_commands(*, simple_recipe_path: Path, config: dict[str, Any]) -> list[str]:
     """Extract commands from a simple recipe file."""
     # read_lines is going to be more efficient I assume...
     lines = simple_recipe_path.read_text().split("\n")
 
+    work_dir = _get_workdir(mount_dir=MOUNT_DIR, id=config["id"])
+
     # Omit comments and empty lines
     commands = [line for line in lines if line and not line.startswith("#")]
-    return commands
 
-
-def _get_input_constructor_and_arg(config: dict[Any, Any]) -> tuple[str, Any]:
-    acceptable_values = f"Acceptable values: {input_subkeys.keys()}"
-    if num_keys := len(config["input"].keys()) > 1:
-        raise RuntimeError(
-            f"Expected 1 sub-key for the `input` key; got {num_keys}."
-            f" {acceptable_values}"
-        )
-
-    key, val = list(config["input"].items())[0]
-
-    try:
-        clss = input_subkeys[key]
-    except KeyError:
-        raise RuntimeError(
-            f"Received unexecpected sub-key for `input` key: {key}"
-            f" {acceptable_values}"
-        )
-
-    return clss, val
+    interpolated_commands = []
+    previous_subdir = work_dir / "fetch"
+    interpolated_commands.append(f"mkdir {previous_subdir}")
+    fetch_cmd = f"curl -o {previous_subdir} {config['input']['url']}"
+    interpolated_commands.append(fetch_cmd)
+    for idx, command in enumerate(commands):
+        output_dir = work_dir / str(idx)
+        interpolated_command = command.format(input_dir=previous_subdir, output_dir=output_dir)
+        interpolated_commands.append(f"mkdir {output_dir}")
+        interpolated_commands.append(interpolated_command)
+        previous_subdir = output_dir
+        
+    return interpolated_commands
 
 
 def render_simple_recipe(recipe_directory: Path) -> None:
-    commands = _get_commands(recipe_directory / SIMPLE_RECIPE_FILENAME)
     config = get_recipe_config(recipe_directory)
-
-    input_constructor, input_constructor_arg = _get_input_constructor_and_arg(config)
+    commands = _get_commands(simple_recipe_path=recipe_directory / SIMPLE_RECIPE_FILENAME, config=config)
 
     print(
         template.render(
             commands=commands,
-            input_constructor=input_constructor,
-            input_constructor_arg=input_constructor_arg,
+            recipe_id=config["id"],
+            pvc_name=PVC_NAME,
+            mount_dir=MOUNT_DIR,
         )
     )
