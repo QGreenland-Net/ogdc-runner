51bd71731e02c8e42dc35c21dc6c3f5b42cde1ee
Matt Fisher <mfisher87@gmail.com>
2024-04-24 18:13:46 -0600

    Stop running mypy with pre-commit
    Reasons we don't want to do this:
    
    * This requires a copy of the dependencies list in the pre-commit config
    * Mypy can be much slower, and the idea of pre-commit is quick feedback
    
    Instead, we're:
    
    * Running mypy with nox
    * Automating in GitHub Actions

--- noxfile.py
+++ noxfile.py
@@ -9,35 +9,27 @@ import nox
 DIR = Path(__file__).parent.resolve()
 
 nox.needs_version = ">=2024.3.2"
-nox.options.sessions = ["lint", "tests"]
+nox.options.sessions = ["typecheck", "tests"]
 nox.options.default_venv_backend = "uv|virtualenv"
 
 
 @nox.session
-def lint(session: nox.Session) -> None:
-    """
-    Run the linter.
-    """
-    session.install("pre-commit")
-    session.run(
-        "pre-commit", "run", "--all-files", "--show-diff-on-failure", *session.posargs
-    )
+def typecheck(session: nox.Session) -> None:
+    """Run typechecker."""
+    session.install(".[test]")
+    session.run("mypy", *session.posargs)
 
 
 @nox.session
 def tests(session: nox.Session) -> None:
-    """
-    Run the unit and regular tests.
-    """
+    """Run the unit and regular tests."""
     session.install(".[test]")
     session.run("pytest", *session.posargs)
 
 
 @nox.session(reuse_venv=True)
 def docs(session: nox.Session) -> None:
-    """
-    Build the docs. Pass "--serve" to serve. Pass "-b linkcheck" to check links.
-    """
+    """Build the docs. Pass "--serve" to serve. Pass "-b linkcheck" to check links."""
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--serve", action="store_true", help="Serve after building")
@@ -77,9 +69,7 @@ def docs(session: nox.Session) -> None:
 
 @nox.session
 def build_api_docs(session: nox.Session) -> None:
-    """
-    Build (regenerate) API docs.
-    """
+    """Build (regenerate) API docs."""
 
     session.install("sphinx")
     session.chdir("docs")
@@ -96,9 +86,7 @@ def build_api_docs(session: nox.Session) -> None:
 
 @nox.session
 def build(session: nox.Session) -> None:
-    """
-    Build an SDist and wheel.
-    """
+    """Build an SDist and wheel."""
 
     build_path = DIR.joinpath("build")
     if build_path.exists():
