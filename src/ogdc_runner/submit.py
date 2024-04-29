80475321d3d99805884b7269bb1b97eeb8153c56
Trey Stafford <trey.stafford@colorado.edu>
2024-04-29 16:30:08 -0600

    Reorder `submit` for readability

--- src/ogdc_runner/submit.py
+++ src/ogdc_runner/submit.py
@@ -16,21 +16,21 @@ def submit_recipe(recipe_path: Path) -> None:
     driver_script = render_simple_recipe(recipe_path)
     recipe_config = get_recipe_config(recipe_directory=recipe_path)
 
-    config.load_kube_config()
-    k8s_client = client.ApiClient()
-    recipe_id = recipe_config.id
-
     # A ConfigMap provides the driver script to the cluster
     configmap_manifest_template = j2_environment.get_template("configmap.yml.j2")
     configmap_manifest_yaml = configmap_manifest_template.render(
-        recipe_id=recipe_id,
+        recipe_id=recipe_config.id,
         driver_script=driver_script,
     )
     configmap_manifest = yaml.safe_load(configmap_manifest_yaml)
 
     # A Job executes the driver script on the cluster
     job_manifest_template = j2_environment.get_template("job.yml.j2")
-    job_manifest = yaml.safe_load(job_manifest_template.render(recipe_id=recipe_id))
+    job_manifest = yaml.safe_load(job_manifest_template.render(recipe_id=recipe_config.id))
+
+    # Load k8s config and create an API client instance.
+    config.load_kube_config()
+    k8s_client = client.ApiClient()
 
     # TODO: There is no `apply_from_yaml`, but we really want to apply (i.e. declarative
     # config instead of imperative; shouldn't have to delete a configmap and create it
