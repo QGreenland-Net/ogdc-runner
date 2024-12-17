26a7c6af3d6dde8bec7cc8dcdba91870a75c7dbb
Trey Stafford <trey.stafford@colorado.edu>
2024-12-17 10:46:13 -0700

    OGDC simple workflow integration test: cleanup workflow
    Cleans up the argo workflow after a success. Assuming we'll want to investigate
    logs of any workflows that fail.

--- tests/integration/test_argo_workflow.py
+++ tests/integration/test_argo_workflow.py
@@ -8,6 +8,7 @@ from __future__ import annotations
 
 from pathlib import Path
 
+from ogdc_runner.argo import ARGO_WORKFLOW_SERVICE
 from ogdc_runner.recipe.simple import make_simple_workflow
 
 SIMPLE_RECIPE_TEST_PATH = Path(__file__).parent / "test_recipe"
@@ -18,3 +19,6 @@ def test_simple_argo_workflow():
     submitted_wf = workflow.create(wait=True)
     assert submitted_wf.status  # type: ignore[union-attr]
     assert submitted_wf.status.phase == "Succeeded"  # type: ignore[union-attr]
+    # Cleanup the test workflow
+    if workflow.name:
+        ARGO_WORKFLOW_SERVICE.delete_workflow(workflow.name)
