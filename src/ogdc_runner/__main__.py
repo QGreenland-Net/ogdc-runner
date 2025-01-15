d31e9b2413add409c201a356dc2cc40f3b748503
Trey Stafford <trey.stafford@colorado.edu>
2025-01-15 09:05:34 -0700

    Remove `submit-and-wait` cli: add `--wait` option to `submit` CLI
    Simplifies the interface

--- src/ogdc_runner/__main__.py
+++ src/ogdc_runner/__main__.py
@@ -5,13 +5,6 @@ import click
 from ogdc_runner.argo import get_workflow_status, submit_workflow
 from ogdc_runner.recipe.simple import make_simple_workflow
 
-recipe_path = click.argument(
-    "recipe_path",
-    required=True,
-    metavar="RECIPE-PATH",
-    type=str,
-)
-
 
 @click.group
 def cli() -> None:
@@ -19,8 +12,19 @@ def cli() -> None:
 
 
 @cli.command
-@recipe_path
-def submit(recipe_path: str) -> None:
+@click.argument(
+    "recipe_path",
+    required=True,
+    metavar="RECIPE-PATH",
+    type=str,
+)
+@click.option(
+    "--wait",
+    is_flag=True,
+    default=False,
+    help="Wait for recipe execution to complete.",
+)
+def submit(recipe_path: str, wait: bool) -> None:
     """
     Submit a recipe to OGDC for execution.
 
@@ -30,7 +34,7 @@ def submit(recipe_path: str) -> None:
     workflow = make_simple_workflow(
         recipe_dir=recipe_path,
     )
-    submit_workflow(workflow)
+    submit_workflow(workflow, wait=wait)
 
 
 @cli.command
@@ -43,18 +47,3 @@ def check_workflow_status(workflow_name: str) -> None:
     """Render and submit a recipe to OGDC for execution."""
     status = get_workflow_status(workflow_name)
     print(f"Workflow {workflow_name} has status {status}.")
-
-
-@cli.command
-@recipe_path
-def submit_and_wait(recipe_path: str) -> None:
-    """
-    Submit a recipe to OGDC for execution and wait until completion.
-
-    RECIPE-PATH: Path to the recipe file. Use either a local path (e.g., '/ogdc-recipes/recipes/seal-tags')
-    or an fsspec-compatible GitHub string (e.g., 'github://qgreenland-net:ogdc-recipes@main/recipes/seal-tags').
-    """
-    workflow = make_simple_workflow(
-        recipe_dir=recipe_path,
-    )
-    submit_workflow(workflow, wait=True)
