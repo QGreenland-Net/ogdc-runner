0e1d2659f1db1344d62ad539b8f82e7bfbea53e0
Trey Stafford <trey.stafford@colorado.edu>
2025-10-03 10:46:10 -0600

    Remove unused jinja module
    THis is a remnant of an earlier spike on OGDC work. Having this present causes
    issues with the docs build

--- src/ogdc_runner/jinja.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-from jinja2 import Environment, PackageLoader, StrictUndefined
-
-j2_environment = Environment(
-    loader=PackageLoader("ogdc_runner"), undefined=StrictUndefined
-)
