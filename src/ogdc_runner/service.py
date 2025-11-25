00515aeb24fbf8e532aa502680ca2d9c58879282
Trey Stafford <trey.stafford@colorado.edu>
2025-11-25 15:28:23 -0700

    Service: Add endpoint for version. Root response is docs.

--- src/ogdc_runner/service.py
+++ src/ogdc_runner/service.py
@@ -7,12 +7,19 @@ into one or more Argo workflows that are executed.
 
 from __future__ import annotations
 
+import pydantic
 from fastapi import FastAPI
 
-app = FastAPI()
+from ogdc_runner import __version__
+
+app = FastAPI(docs_url="/")
 
 
-@app.get("/")
-def hello() -> dict[str, str]:
-    """Proof of concept hello-world route."""
-    return {"message": "Hello World!"}
+class VersionResponse(pydantic.BaseModel):
+    ogdc_runner_version: str = __version__
+
+
+@app.get("/version")
+def version() -> VersionResponse:
+    """Return the OGDC runner version."""
+    return VersionResponse()
--- tests/unit/test_service.py
+++ tests/unit/test_service.py
@@ -2,12 +2,13 @@ from __future__ import annotations
 
 from fastapi.testclient import TestClient
 
+from ogdc_runner import __version__
 from ogdc_runner.service import app
 
 client = TestClient(app)
 
 
 def test_root():
-    response = client.get("/")
+    response = client.get("/version")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World!"}
+    assert response.json() == {"ogdc_runner_version": __version__}
