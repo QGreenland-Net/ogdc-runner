91c08f92a76c51b48e5b6c2345cbdccb500ffea9
Trey Stafford <trey.stafford@colorado.edu>
2025-12-10 17:11:57 -0700

    Make mock db logic into pytest fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
+from functools import cache
 from pathlib import Path
 
 import pytest
+from sqlmodel import create_engine
+
+from ogdc_runner import db
 
 SHELL_RECIPE_TEST_PATH = Path(__file__).parent / "test_shell_workflow_recipe_dir"
 VIZ_RECIPE_TEST_PATH = Path(__file__).parent / "test_viz_workflow_recipe_dir"
@@ -16,3 +20,13 @@ def test_shell_workflow_recipe_directory() -> Path:
 @pytest.fixture
 def test_viz_workflow_recipe_directory() -> Path:
     return VIZ_RECIPE_TEST_PATH
+
+
+@pytest.fixture
+def mock_db(monkeypatch):
+    @cache
+    def mock_get_engine():
+        return create_engine("sqlite:///:memory:")
+
+    monkeypatch.setattr(db, "_get_engine", mock_get_engine)
+    monkeypatch.setenv("OGDC_ADMIN_PASSWORD", "password")
--- tests/unit/test_service.py
+++ tests/unit/test_service.py
@@ -1,21 +1,12 @@
 from __future__ import annotations
 
-from functools import cache
-
 from fastapi.testclient import TestClient
-from sqlmodel import create_engine
 
-from ogdc_runner import __version__, db
+from ogdc_runner import __version__
 from ogdc_runner.service import app
 
 
-def test_version(monkeypatch):
-    @cache
-    def mock_get_engine():
-        return create_engine("sqlite:///:memory:")
-
-    monkeypatch.setattr(db, "_get_engine", mock_get_engine)
-    monkeypatch.setenv("OGDC_ADMIN_PASSWORD", "password")
+def test_version(mock_db):  # noqa: ARG001
     with TestClient(app) as client:
         response = client.get("/version")
         assert response.status_code == 200
