a1ecad6a212093877d6c6fd768659f6cbe4174a0
Trey Stafford <trey.stafford@colorado.edu>
2025-12-16 10:41:39 -0700

    Setup `auth_routes` module with all routes requiring auth by default

--- src/ogdc_runner/service/main.py
+++ src/ogdc_runner/service/main.py
@@ -7,18 +7,14 @@ into one or more Argo workflows that are executed.
 
 from __future__ import annotations
 
-import datetime as dt
 from contextlib import asynccontextmanager
 
 import pydantic
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from loguru import logger
 
 from ogdc_runner import __version__
-from ogdc_runner.api import submit_ogdc_recipe
-from ogdc_runner.argo import get_workflow_status
-from ogdc_runner.recipe import stage_ogdc_recipe
-from ogdc_runner.service import auth
+from ogdc_runner.service import auth, auth_routes
 from ogdc_runner.service.db import (
     close_db,
     init_db,
@@ -52,6 +48,7 @@ app = FastAPI(
 )
 
 app.include_router(auth.router)
+app.include_router(auth_routes.router)
 
 
 class VersionResponse(pydantic.BaseModel):
@@ -62,70 +59,3 @@ class VersionResponse(pydantic.BaseModel):
 def version() -> VersionResponse:
     """Return the OGDC runner version."""
     return VersionResponse()
-
-
-class SubmitRecipeRequest(pydantic.BaseModel):
-    recipe_path: str
-    overwrite: bool = False
-
-
-class SubmitRecipeResponse(pydantic.BaseModel):
-    message: str
-    recipe_workflow_name: str | None
-
-
-@app.post("/submit")
-def submit(
-    submit_recipe_request: SubmitRecipeRequest,
-    # Ensure submissions require an authenticated user.
-    _current_user: auth.AuthenticatedUserDependency,
-) -> SubmitRecipeResponse:
-    """Submit a recipe to OGDC for execution.
-
-    Requires a valid auth token.
-    """
-    try:
-        with stage_ogdc_recipe(submit_recipe_request.recipe_path) as recipe_dir:
-            recipe_workflow_name = submit_ogdc_recipe(
-                recipe_dir=recipe_dir,
-                # Submitting a recipe should never wait - the api should be
-                # responsive and async.
-                wait=False,
-                overwrite=submit_recipe_request.overwrite,
-            )
-            return SubmitRecipeResponse(
-                message=f"Successfully submitted recipe with {recipe_workflow_name=}",
-                recipe_workflow_name=recipe_workflow_name,
-            )
-    except Exception as e:
-        raise HTTPException(
-            status_code=500,
-            detail=f"Failed to submit recipe with error: {e}.",
-        ) from e
-
-
-class StatusResponse(pydantic.BaseModel):
-    recipe_workflow_name: str
-    status: str | None
-    timestamp: dt.datetime = pydantic.Field(default_factory=dt.datetime.now)
-
-
-@app.get("/status/{recipe_workflow_name}")
-def status(recipe_workflow_name: str) -> StatusResponse:
-    """Check an argo workflow's status."""
-    status = get_workflow_status(recipe_workflow_name)
-    return StatusResponse(
-        recipe_workflow_name=recipe_workflow_name,
-        status=status,
-    )
-
-
-@app.get("/user")
-async def get_current_user(
-    current_user: auth.AuthenticatedUserDependency,
-) -> dict[str, str]:
-    """Return the current authenticated user.
-
-    Useful for testing that authentication is working as expected.
-    """
-    return {"current_user": current_user.name}
