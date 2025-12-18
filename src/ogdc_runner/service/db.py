244fd57fc996d11498ead966e1c50450a1c64234
Trey Stafford <trey.stafford@colorado.edu>
2025-12-18 14:58:53 -0700

    Move user-specific code into its own module

--- src/ogdc_runner/service/db.py
+++ src/ogdc_runner/service/db.py
@@ -1,8 +1,4 @@
-"""Module containing code for interacting with the OGDC database.
-
-Defines database tables, configures a connection to the database, and includes
-user creation code.
-"""
+"""Module containing code for interacting with the OGDC database."""
 
 from __future__ import annotations
 
@@ -14,20 +10,10 @@ from typing import Annotated
 import sqlalchemy
 from fastapi import Depends
 from loguru import logger
-from pwdlib import PasswordHash
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Session, SQLModel, create_engine
 
-from ogdc_runner.exceptions import OgdcMissingEnvvar, OgdcUserAlreadyExists
-
-
-class User(SQLModel, table=True):
-    """Model representing the `users` table in the OGDC database."""
-
-    __tablename__ = "users"
-    # `id` field is auto-created
-    id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-    password_hash: str
+from ogdc_runner.exceptions import OgdcMissingEnvvar
+from ogdc_runner.service.user import create_admin_user
 
 
 @cache
@@ -60,82 +46,11 @@ def close_db() -> None:
     get_engine().dispose()
 
 
-def get_user(*, session: Session, name: str) -> User | None:
-    results = session.exec(select(User).where(User.name == name)).one_or_none()
-
-    return results
-
-
-def get_user_with_password(
-    *,
-    session: Session,
-    name: str,
-    password: str,
-) -> None | User:
-    """Get the user record by name and password."""
-    user = get_user(session=session, name=name)
-    if user is None or not verify_password(
-        password=password, hashed_password=user.password_hash
-    ):
-        return None
-
-    return user
-
-
-password_hash = PasswordHash.recommended()
-
-
-def hash_password(password: str) -> str:
-    return password_hash.hash(password)
-
-
-def verify_password(*, password: str, hashed_password: str) -> bool:
-    return password_hash.verify(password, hashed_password)
-
-
-def create_user(*, username: str, password: str) -> User:
-    """Create a user with the given username and password.
-
-    Raises `OgdcUserAlreadyExists` if the given username is already in use.
-    """
-    with Session(get_engine()) as session:
-        user = get_user(session=session, name=username)
-        if user:
-            err_msg = f"User with {username=} already created."
-            raise OgdcUserAlreadyExists(err_msg)
-
-        new_user = User(
-            name="admin",
-            password_hash=hash_password(password),
-        )
-        session.add(new_user)
-        session.commit()
-
-    logger.info(f"User {username} created.")
-
-    return new_user
-
-
-def create_admin_user() -> None:
-    """Create the admin user if it does not already exist.
-
-    Requires that the `OGDC_ADMIN_PASSWORD` envvar be set.
-    """
-    admin_password = os.environ.get("OGDC_ADMIN_PASSWORD")
-    if not admin_password:
-        err_msg = "`OGDC_ADMIN_PASSWORD` envvar must be set."
-        raise OgdcMissingEnvvar(err_msg)
-
-    try:
-        create_user(username="admin", password=admin_password)
-    except OgdcUserAlreadyExists:
-        logger.info("Admin user already created.")
-
-
 def init_db() -> None:
     """Initialize the database with tables and an admin user."""
     logger.info("Ensuring database is ready on app startup...")
     SQLModel.metadata.create_all(get_engine())
     logger.info("Database tables are ready.")
-    create_admin_user()
+    with Session(get_engine()) as session:
+        create_admin_user(session=session)
     logger.info("Admin user is created.")
